5177657ce6f5d224ec5fbcc2a1b985f26df4ca75
Diego Escala <diego.escala@yahoo.com>
2020-03-22 13:27:42 -0500

    Added currency localization and improved explanation

--- src/abakus/edit_prop.cljs
+++ src/abakus/edit_prop.cljs
@@ -25,28 +25,61 @@
                                   (swap! prop-info assoc param (if (not (empty? (str %))) (js/parseFloat %) ""))
                                   (reset! computed (analyzer/recompute (scrub @prop-info))))}]]])
 
+(defn localize
+  [cur]
+  (let [f (js/Intl.NumberFormat. "en-US" (clj->js {:style "currency" :currency "USD"}))]
+    (-> f (.format cur))))
+
+(defn localize-currency-vals
+  [m]
+  (let [localized (reduce (fn [a [k v]]
+                            (assoc a k (localize v)))
+                          {}
+                          m)]
+    (println (prn-str localized))
+    localized))
+
+(defn explanation
+  [prop]
+  (let [{:keys [total-cost down five-yr-profit closing-costs rehab five-yr-apprec mortgage cash-flow-per-unit stock-market-ret]} prop]
+    (str "With a down payment of " down
+         ", closing costs of " closing-costs ", and estimated rehab costs of " rehab
+         ", you would be putting in " total-cost " in cash. "
+         "Considering a projected cash flow/month of " cash-flow-per-unit ", "
+         "and 5-year appreciation of " five-yr-apprec ", you could expect to make about "
+         five-yr-profit " over the next 5 years. If you put your " total-cost " into the stock market instead, "
+         "you would make " stock-market-ret " over the next 5 years.")))
 
 (defn summary-header
-  []
-  (let [vs-mkt (:mkt-beat @computed)
-        cash-in (:total-cost @computed)
+  [prop]
+  (let [vs-mkt (:mkt-beat prop)
+        cash-in (:total-cost prop)
+        cash-flow (:cash-flow-per-unit prop)
         good? (pos? vs-mkt)]
-    [rn/view {:style {:margin-top 0 :background-color "#96ccb8"  :flex-direction "row" :align-text "center"}}
-     [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
-      [rn/text {:style {:color "#fff" :font-size 20}}
-       "Good Deal?"]
-      [rn/text {:style {:font-size 25}}
-       (if good? "Yes" "No")]]
-     [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
-      [rn/text {:style {:color "#fff" :font-size 20}}
-       "Cash In"]
-      [rn/text {:style {:font-size 25}}
-       (Math/floor cash-in)]]
-     [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
-      [rn/text {:style {:color "#fff" :font-size 20}}
-       "Return vs Stock Market"]
-      [rn/text {:style {:font-size 25}}
-       (Math/floor vs-mkt)]]]))
+    [rn/view {:style {:flex-direction "column" :background-color "#96ccb8"}}
+     [rn/view {:style {:margin-top 0   :flex-direction "row" :align-text "center"}}
+      [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
+       [rn/text {:style {:color "#fff" :font-size 20}}
+        "Good Deal?"]
+       (if good?
+         [rn/text {:style {:font-size 25 :color "green"}}
+          "Yes"]
+         [rn/text {:style {:font-size 25 :color "red"}}
+          "No"])]
+      [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
+       [rn/text {:style {:color "#fff" :font-size 20}}
+        "Cash In"]
+       [rn/text {:style {:font-size 25}}
+        (localize cash-in)]]
+      [rn/view {:style {:margin-left 20 :margin-top 30 :flex-direction "column" :align-text "center"}}
+       [rn/text {:style {:color "#fff" :font-size 20}}
+        "Return vs Stock Market"]
+       [rn/text {:style {:font-size 25}}
+        (localize vs-mkt)]]]
+     [rn/view
+       [rn/touchable-highlight
+          {:on-press #(rn/alert (explanation (localize-currency-vals prop)))}
+         [rn/text "Explanation"]]]]))
 
 
 (defn basic-questions
@@ -66,5 +99,5 @@
   [prop]
   (swap! prop-info merge prop @prop-info)
   [rn/view
-   [summary-header]
+   [summary-header @computed]
    [basic-questions]])
