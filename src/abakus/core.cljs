c20dd8008cec57b23de0e405e376d57a850e28d3
Diego Escala <diego.escala@yahoo.com>
2020-04-11 17:18:07 -0500

    Added what-if chart to core

--- src/abakus/core.cljs
+++ src/abakus/core.cljs
@@ -5,21 +5,34 @@
               [abakus.rn :as rn]
               [abakus.handlers]
               [abakus.subs]
-              [abakus.edit-prop :as edit-prop]))
+              [abakus.edit-prop :as edit-prop]
+
+              [abakus.analyzer :as anal]))
 
 
-(def line (clj->js {:labels ["January" "February" "March"]
-                    :datasets [{:data [20 45 28]
+(def base-prop {:purchase-price 140000 :hoa 271 :rehab 2000})
+
+(def cfs [1200 1400 1600 1800 2000 2200])
+
+(def evals (reduce #(conj %1 (anal/recompute (assoc base-prop :rent-per-unit %2))) [] cfs))
+
+(def line (clj->js {:labels (map str cfs)
+                    :datasets [{:data (map :cash-flow-per-unit evals)
                                 :strokeWidth 2}]}))
 
-(def show-chart (r/atom false))
+(def show-chart (r/atom true))
 
 (defn app-root []
   (js/console.log line)
   (if @show-chart
-    [rn/view {:flex 3}
+    [rn/view {:flex 3 :style {:margin-top 50}}
+     [rn/text "Cash flow per unit"]
      [rn/line-chart {:data line :width 300 :height 300
-                     :chartConfig {:color (fn [o] "#99999999")}}]]
+                     :chartConfig {:color (fn [o] "rgba(255,255,255,1)")
+                                   :backgroundColor "#e26a00"
+                                   :backgroundGradientFrom "#fb8c00"
+                                   :backgroundGradientTo "#ffa726"
+                                   :decimalPlaces 0}}]]
     [edit-prop/edit-prop {}]))
 
 (defn init []
