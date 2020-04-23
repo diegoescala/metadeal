538a9b6a9841603703bac6840ee103321d3222aa
Diego Escala <diego.escala@yahoo.com>
2020-04-22 23:00:25 -0500

    Added direction control to targeter

--- src/abakus/targeter.cljs
+++ src/abakus/targeter.cljs
@@ -2,7 +2,7 @@
   (:require [abakus.analyzer :as analyzer]))
 
 (defn compute-target
-  [prop variable target-metric target-value tolerance]
+  [prop variable target-metric target-value tolerance direction]
   (loop [lo 0
          hi 100000000]
     (let [test-value (* 0.5 (+ lo hi))
@@ -12,10 +12,10 @@
       ; (println (prn-str [test-value metric-value target-value lo hi]))
       (if (> tolerance (Math/abs (- metric-value target-value)))
         test-value
-        (let [new-lo (if (> metric-value target-value) lo test-value)
-              new-hi (if (> metric-value target-value) test-value hi)]
+        (let [new-lo (if (direction metric-value target-value) lo test-value)
+              new-hi (if (direction metric-value target-value) test-value hi)]
           (recur new-lo new-hi))))))
 
-(def prop {:purchase-price 140000 :hoa 271 :num-units 2})
+(def prop {:hoa 271 :num-units 1 :rent-per-unit 1260 :rehab 1000 :vacancy-percentage 0})
 
-(compute-target prop :rent-per-unit :cash-flow-per-unit 100 5)
+(compute-target prop :purchase-price :cash-flow-per-unit 100 5 <)
