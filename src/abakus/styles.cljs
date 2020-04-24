7963ee12d2464d64f58d13436a12471327b8845a
Diego Escala <diego.escala@yahoo.com>
2020-04-24 00:04:18 -0500

    Centered navbar option text

--- src/abakus/styles.cljs
+++ src/abakus/styles.cljs
@@ -20,7 +20,7 @@
 
 (def navbar-container {:min-height 30 :padding-top 40 :background-color (:dark-purple app-colors) :flex-direction :row :align-items :center})
 (def navbar-option {:flex 1 :border-right-width 1 :border-right-color (:white app-colors)})
-(def navbar-text {:color (:white app-colors) :font-weight :600})
+(def navbar-text {:color (:white app-colors) :text-align :center :font-weight :600})
 
 (def app-main {:flex-direction "column"})
 
