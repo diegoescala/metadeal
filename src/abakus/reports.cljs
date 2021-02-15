385be39156361655283957f30657c5a75355b432
Diego Escala <diego.escala@yahoo.com>
2021-02-14 18:51:32 -0600

    Added ads to reports page, improved report styling, and made the page scrollable

--- src/abakus/reports.cljs
+++ src/abakus/reports.cljs
@@ -1,7 +1,8 @@
 (ns abakus.reports
   (:require [abakus.analyzer :as anal]
-            [abakus.rn :as rn]))
-
+            [abakus.rn :as rn]
+            [abakus.ads :as ads]
+            [abakus.styles :as styles]))
 
 (def base-prop {:purchase-price 140000 :hoa 271 :rehab 2000})
 
@@ -15,11 +16,26 @@
 
 (defn report
   []
-  [rn/view {:flex 3 :style {:margin-top 50}}
-   [rn/text "Cash flow per unit"]
-   [rn/line-chart {:data line :width 300 :height 300
-                   :chartConfig {:color (fn [o] "rgba(255,255,255,1)")
-                                 :backgroundColor "#e26a00"
-                                 :backgroundGradientFrom "#fb8c00"
-                                 :backgroundGradientTo "#ffa726"
-                                 :decimalPlaces 0}}]])
+  [rn/view {:style (merge {:flex 1} styles/app-screen)}
+   [rn/view {:style (merge styles/container {:flex-direction :column :flex 1})}
+    [rn/view {:style styles/screen-header}
+     [rn/view {:style styles/good-deal-container}
+      [rn/text {:style styles/screen-title-text}
+       "My Properties"]]
+     [rn/scroll-view
+      [ads/banner]
+      [rn/text "Cash flow per unit"]
+      [rn/line-chart {:data line :width 300 :height 300
+                      :chartConfig {:color (fn [o] "rgba(255,255,255,1)")
+                                    :backgroundColor (:dark-purple styles/app-colors)
+                                    :backgroundGradientFrom (:dark-purple styles/app-colors)
+                                    :backgroundGradientTo (:dark-purple styles/app-colors)
+                                    :decimalPlaces 0}}]
+      [ads/banner]
+      [rn/text "Cash flow per unit"]
+      [rn/line-chart {:data line :width 300 :height 300
+                      :chartConfig {:color (fn [o] "rgba(255,255,255,1)")
+                                    :backgroundColor (:dark-purple styles/app-colors)
+                                    :backgroundGradientFrom (:dark-purple styles/app-colors)
+                                    :backgroundGradientTo (:dark-purple styles/app-colors)
+                                    :decimalPlaces 0}}]]]]])
